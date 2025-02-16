2d0228efefd23037e65d1084ecb2f3dc23911151
mahmudurmahid <m.mahid@icloud.com>
2025-02-16 21:57:57 +0000

    react set up, lots of debugging

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-8 bg-white shadow-lg rounded-lg">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind CSS!
+        </h1>
+        <p className="mt-4 text-gray-700">Your setup is working! 🎉</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
